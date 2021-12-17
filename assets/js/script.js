6b4091df4ede7bc4097a6da3696a5749c224aff2
suzybee1987 <suzannemiller1987@gmail.com>
2021-12-17 17:09:34 +0000

    Dev: add function on Dom load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
+// Wait for the DOM to finish loading before running the game 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    // This returns buttons as an array 
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+});
+
+// The main game loop, called when the script is 
+// first loaded and after the users answer has been processed 
 function runGame() {
+    let num1 = Math.floor(Math.random() *25) + 1
+    let num2 = Math.floor(Math.random() *25) + 1
 
 }
 
