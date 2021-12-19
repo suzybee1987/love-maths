00a789eec7c6cf53e54f2106c789f7dd0a060dbe
suzybee1987 <suzannemiller1987@gmail.com>
2021-12-19 09:24:33 +0000

    Feat: add division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 - operand2, "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
